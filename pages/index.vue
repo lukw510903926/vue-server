032ca091eeab22214d5c568733963dc4d8ff631f
yangqi <13507615840@163.com>
2018-10-15 17:28:35 +0800

    vue nuxt 服务端渲染

---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-row type="flex">
+        <el-col :span=20> vue-nuxt 服务端渲染</el-col>
+        <el-col :span=4 class="login-user">
+          管理员
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside width="220px" v-bind:style="asideStyle">
+        <LeftMenu/>
+      </el-aside>
+      <el-main>
+        <transition name="move" mode="out-in">
+          <router-view/>
+        </transition>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import LeftMenu from './LeftMenu.vue'
+
+export default {
+  components: {
+    LeftMenu
+  },
+  data() {
+    return {
+     asideStyle: {
+          minHeight: 100+'vh'
+        }
+    };
+  },
+  mounted() {},
+  created() {
+  }
+};
+</script>
+<style scoped>
+.login-user {
+  text-align: right;
+  margin-right: 20px;
+}
+</style>
---
+++ plugins/element-ui.js
@@ -0,0 +1,6 @@
+// element-ui.js
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import '../static/css/common.css'
+Vue.use(ElementUI);
