a6a85623eaa4963fee73ed8d0164c63b040f084a
yangqi <13507615840@163.com>
2018-10-16 16:27:50 +0800

    http 请求转发配置

--- pages/LeftMenu.vue
+++ pages/LeftMenu.vue
@@ -3,10 +3,6 @@
            text-color="#fff" active-text-color="#ffd04b" :router=true>
     <el-menu-item index="/home/product" route="/home/product">产品</el-menu-item>
     <el-menu-item index="/home/product/list" route="/home/product/list">产品列表</el-menu-item>
-    <el-menu-item index="/home/tabs" route="/home/tabs">tab列表</el-menu-item>
-    <el-menu-item index="/home/treePage" route="/home/treePage">treePage</el-menu-item>
-    <el-menu-item index="/home/treeGrid" route="/home/treeGrid">treeGrid</el-menu-item>
-    <el-menu-item index="/home/vuex" route="/home/vuex">vuex</el-menu-item>
   </el-menu>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <el-row type="flex">
         <el-col :span=20> vue-nuxt 服务端渲染</el-col>
         <el-col :span=4 class="login-user">
-          管理员
+          {{loginUser}}
         </el-col>
       </el-row>
     </el-header>
@@ -22,27 +22,37 @@
 </template>
 
 <script>
-import LeftMenu from './LeftMenu.vue'
+  import LeftMenu from './LeftMenu.vue'
 
-export default {
-  components: {
-    LeftMenu
-  },
-  data() {
-    return {
-     asideStyle: {
-          minHeight: 100+'vh'
+  export default {
+    components: {
+      LeftMenu
+    },
+    /**
+     * 运行在服务端 页面初始化之前执行 可以用来获取服务端数据
+     * @param context
+     * @returns {{loginUser: string}}
+     */
+    asyncData(context) {
+      return {loginUser: '超级管理员'}
+    },
+    data() {
+      return {
+        asideStyle: {
+          minHeight: 100 + 'vh'
         }
-    };
-  },
-  mounted() {},
-  created() {
-  }
-};
+      };
+    },
+    mounted() {
+      this.$http.getRequest("/user/list").then(response =>console.log('data:',response));
+    },
+    created() {
+    }
+  };
 </script>
 <style scoped>
-.login-user {
-  text-align: right;
-  margin-right: 20px;
-}
+  .login-user {
+    text-align: right;
+    margin-right: 20px;
+  }
 </style>
